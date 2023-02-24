74b72fa777ec01ea842b86eab5047f3bc31c493d
Julie Moura <juliemouradev@gmail.com>
2023-02-24 12:51:50 -0300

    update: improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { CodextTheme } from "@themes/CodextTheme";
-import { Button } from "@components/buttons";
+import { Primary } from "@components/buttons";
 import { Typography } from "@components/typography";
 
 export default function Home() {
   return (
     <ThemeProvider theme={CodextTheme}>
-      <h1>Codext Page</h1>
+      <Typography fontWeight="regular" fontSize="fs200">Codext Page</Typography>
+      <Primary>Try a free trial</Primary>
     </ThemeProvider>
   )
 }
