ee09758eb2f71c64cb5b785c6fa29daa9d7629e3
Julie Moura <juliemouradev@gmail.com>
2023-02-24 12:14:42 -0300

    feat: add components

---
+++ themes/CodextTheme.ts
@@ -0,0 +1,56 @@
+export const CodextTheme = {
+  colors: {
+    transparent: "transparent",
+    white: "#FFFFFF",
+    black: "#000000",
+    green: "#5EBD14",
+    pumpkin: "#EF821E",
+    red: "#AF3E3E",
+    pink: "#FAEAEA",
+
+    // purple
+    purple100: "#F4E7F6",
+    purple200: "#C333DB",
+    purple500: "#551BA9",
+
+    // blue
+    blue100: "#E7F6F5",
+    blue200: "#D9F0FC",
+    blue300: "#43B4EE",
+    blue400: "#56A39E",
+    blue500: "#051114",
+
+    // brown
+    brown100: "#F9F2E8",
+    brown200: "#9B6717",
+  },
+  fontSize: {
+    fs50: "0.625rem", //10px
+    fs75: "0.813rem", //13px
+    fs100: "1rem", //16px
+    fs200: "1.25rem", //20px
+    fs300: "1.563rem", //25px
+    fs420: "2rem", //32px
+    fs450: "2.25rem", // 36px
+    fs550: "2.5rem", // 40px
+    fs600: "3.063rem", //49px
+    fs700: "3.813rem", //61px
+    fs750: "4rem", //64px
+    fs800: "4.75rem", //76px
+    fs900: "5.938rem", //95px
+  },
+  fontWeight: {
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+  },
+  lineHeight: {
+    fs50: "16px",
+    fs75: "16px",
+    fs100: "24px",
+  },
+  fontFamily: {
+    ubuntu: "'Ubuntu', sans-serif",
+  },
+};
