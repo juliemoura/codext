2b3b03f1465a6c8d342baf82c35134fc6c61ef59
Julie Moura <juliemouradev@gmail.com>
2023-02-28 13:06:16 -0300

    feat: add inferior content

--- components/navbar/inferiorContent/index.tsx
+++ components/navbar/inferiorContent/index.tsx
@@ -1,9 +1,23 @@
-import { Container } from "./styles";
+import { Principal } from "@assets/svgs/Principal";
+import { Primary } from "@components/buttons";
+import { Typography } from "@components/typography";
+import { Container, LeftContainer, RightContainer } from "./styles";
 
 const InferiorContent = () => {
-    return(
+    return (
         <Container>
-            Inferior
+            <LeftContainer>
+                <Typography fontSize="fs600" fontWeight="bold">
+                    Codext one of the best system in SAAS.
+                </Typography>
+                <Typography fontSize="fs100" fontFamily="inter" fontWeight="regular">
+                    Codext is a leading SAAS system that has revolutionized the way businesses manage their operations. With its intuitive interface and comprehensive features, Codext has established itself as the best SAAS system in the market.
+                </Typography>
+                <Primary>Try A Live Demo</Primary>
+            </LeftContainer>
+            <RightContainer>
+                <Principal width={500} height={500} />
+            </RightContainer>
         </Container>
     )
 }
--- components/navbar/inferiorContent/styles.ts
+++ components/navbar/inferiorContent/styles.ts
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    border: 1px solid blue;
+    display: flex;
+    justify-content: space-between;
 `;
+
+export const LeftContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    gap: 30px;
+    width: 40%;
+`;
+
+export const RightContainer = styled.div``;
